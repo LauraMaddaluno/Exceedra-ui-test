a5f665e2b39740e98c1dca8a89e27a0ddfd95258
LauraMaddaluno <lauralondon.4@gmail.com>
2022-01-26 15:35:30 +0000

    display list of players from api

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function Welcome() {
-  return <section>Welcome</section>;
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const response = await fetch("http://localhost:5000/players");
+      const data = await response.json();
+      console.log(data);
+      setPlayers(data);
+    };
+    fetchPlayers();
+  }, []);
+
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <hr></hr>
+      {players.map((player) => (
+        <div>
+          {player.name} {player.surname} {player.nationality} {player.age}
+          {player.points}
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default Welcome;
