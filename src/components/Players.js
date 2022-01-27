0174edcc842acf9fbece0512c4c954d5a0a7c4e8
LauraMaddaluno <lauralondon.4@gmail.com>
2022-01-27 18:31:21 +0000

    starting bootstrap table

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,11 +1,33 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
 const Players = ({ player }) => {
   return (
-    <section>
-      {player.name} {player.surname} {player.nationality} {player.age}{" "}
-      {player.points}
-    </section>
+    <div>
+      <Table striped bordered hover>
+        {/* <thead>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Nationality</th>
+            <th>Age</th>
+            <th>Points</th>
+          </tr>
+        </thead> */}
+        <tbody>
+          <tr>
+            <td>{player.name}</td>
+            <td>{player.surname}</td>
+            <td>{player.nationality}</td>
+            <td>{player.age}</td>
+            <td>{player.points}</td>
+          </tr>
+        </tbody>
+
+        {/* {player.name} {player.surname} {player.nationality} {player.age}{" "}
+        {player.points} */}
+      </Table>
+    </div>
   );
 };
 
