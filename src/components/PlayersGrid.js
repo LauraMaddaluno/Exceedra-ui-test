bbdad0d83c3277c3fdb285e96a0f9bdb50e42c81
LauraMaddaluno <lauralondon.4@gmail.com>
2022-01-29 17:12:39 +0000

    add sort icon

--- src/components/PlayersGrid.js
+++ src/components/PlayersGrid.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaSort } from "react-icons/fa";
 
 const PlayersGrid = ({ players, sorting }) => {
   return (
@@ -6,12 +7,24 @@ const PlayersGrid = ({ players, sorting }) => {
       <table className="table table-bordered">
         <thead>
           <tr>
-            <th onClick={() => sorting("id")}>Ranking</th>
-            <th onClick={() => sorting("name")}>First Name</th>
-            <th onClick={() => sorting("surname")}>Surname</th>
-            <th onClick={() => sorting("nationality")}>Nationality</th>
-            <th onClick={() => sorting("age")}>Age</th>
-            <th onClick={() => sorting("points")}>Points</th>
+            <th onClick={() => sorting("id")}>
+              Ranking <FaSort />
+            </th>
+            <th onClick={() => sorting("name")}>
+              First Name <FaSort />
+            </th>
+            <th onClick={() => sorting("surname")}>
+              Surname <FaSort />
+            </th>
+            <th onClick={() => sorting("nationality")}>
+              Nationality <FaSort />
+            </th>
+            <th onClick={() => sorting("age")}>
+              Age <FaSort />
+            </th>
+            <th onClick={() => sorting("points")}>
+              Points <FaSort />
+            </th>
           </tr>
         </thead>
         <tbody>
